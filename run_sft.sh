14092b760715f901cb130e98381c0057088cb43d
lhkhiem28 <lhkhiem28@gmail.com>
2026-01-05 22:58:02 -0500

    .

--- run_sft.sh
+++ run_sft.sh
@@ -1,7 +1,7 @@
-# python train_sft.py --llm_name 'gemma-2-2b' --batch_size 2 --run_name "sft" --gamma 1.0
-# python test.py      --llm_name 'gemma-2-2b' --batch_size 2 --run_name "sft" --gamma 1.0 --checkpoint_path output/train/DrugADMET/llm_gemma-2-2b_train_8_sft.pth
-# python train_sft.py --llm_name 'granite-3.3-2b' --batch_size 2 --run_name "sft" --gamma 1.0
-# python test.py      --llm_name 'granite-3.3-2b' --batch_size 2 --run_name "sft" --gamma 1.0 --checkpoint_path output/train/DrugADMET/llm_granite-3.3-2b_train_8_sft.pth
+python train_sft.py --llm_name 'gemma-2-2b' --batch_size 2 --run_name "sft" --gamma 1.0
+python test.py      --llm_name 'gemma-2-2b' --batch_size 2 --run_name "sft" --gamma 1.0 --checkpoint_path output/train/DrugADMET/llm_gemma-2-2b_train_8_sft.pth
+python train_sft.py --llm_name 'granite-3.3-2b' --batch_size 2 --run_name "sft" --gamma 1.0
+python test.py      --llm_name 'granite-3.3-2b' --batch_size 2 --run_name "sft" --gamma 1.0 --checkpoint_path output/train/DrugADMET/llm_granite-3.3-2b_train_8_sft.pth
 python train_sft.py --llm_name 'gemma-2-2b' --use_rule --rule_index 0 --batch_size 2 --run_name "sft_fgs_rule" --gamma 1.0
 python test.py      --llm_name 'gemma-2-2b' --use_rule --rule_index 0 --batch_size 2 --run_name "sft_fgs_rule" --gamma 1.0 --checkpoint_path output/train/DrugADMET/llm_gemma-2-2b_train_8_sft_fgs_rule.pth
 python train_sft.py --llm_name 'granite-3.3-2b' --use_rule --rule_index 0 --batch_size 2 --run_name "sft_fgs_rule" --gamma 1.0
